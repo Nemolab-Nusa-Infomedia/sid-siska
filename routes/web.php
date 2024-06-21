787878d8360565bee4b3f24c75a40d7fcc8d2191
vindrayulian <vindrayulian@gmail.com>
2024-06-21 18:29:06 +0700

    add dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ConfigController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ConfigController::class, 'config'])->name('config');
+
+Route::get('/admin', [AdminController::class, 'index'])->name('index');
+
+
+Route::get('/config', [ConfigController::class, 'config'])->name('config');
 Route::get('/form-database', [ConfigController::class, 'configDatabase'])->name('config-database');
 Route::get('/confirm-database', [ConfigController::class, 'confirmDatabase'])->name('confirm-database');
 Route::get('/form-akun', [ConfigController::class, 'configAkun'])->name('config-akun');
